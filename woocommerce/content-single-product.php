19d2be781069470a51227839c2e56cdd17ef9c3e
Okonai <kovacs.ajtai.tamas@gmail.com>
2016-03-22 17:01:11 +0100

    melóban haladtam

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -80,5 +80,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
 
 </div><!-- #product-<?php the_ID(); ?> -->
-
 <?php do_action( 'woocommerce_after_single_product' ); ?>
+<main class="row">
+    <div class="product-single">
+        <div class="product-image">
+            <?php woocommerce_show_product_images();	?>
+        </div>
+        <div class="product-info">
+            <?php woocommerce_template_single_title() ?>
+            <?php woocommerce_template_single_excerpt() ?>  
+            <button class="hollow button" data-toggle="product-more-info">További információ</button>
+        </div>
+        <div class="product-more-info" id="product-more-info" data-toggler=".open">
+            <?php woocommerce_output_product_data_tabs(); ?>
+        </div>
+        <div class="product-order">
+            <label>Size
+            <select>
+            <option value="husker">Small</option>
+            <option value="starbuck">Medium</option>
+            <option value="hotdog">Large</option>
+            <option value="apollo">Yeti</option>
+            </select>
+            </label>
+            <div class="row">
+            <div class="small-3 columns">
+            <label for="middle-label" class="middle">Quantity</label>
+            </div>
+            <div class="small-9 columns">
+            <input type="text" id="middle-label" placeholder="One fish two fish">
+            </div>
+            </div>
+            <a href="#" class="button large expanded">Buy Now</a>
+            <div class="small secondary expanded button-group">
+            <a class="button">Facebook</a>
+            <a class="button">Twitter</a>
+            <a class="button">Yo</a>
+        </div>
+    </div>
+</main>
